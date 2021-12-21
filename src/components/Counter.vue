6f7c9b2c1a06262119ab4706120a587e7231f1d5
Ahmed <atomcgarry@gmail.com>
2021-12-21 15:17:15 +0000

    basic counter app in vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="increment">+</button>
+  <h2>{{ count }}</h2>
+  <button @click="decrement">-</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: () => {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      return (this.count += 1);
+    },
+    decrement() {
+      return (this.count -= 1);
+    },
+  },
+};
+</script>
+
+<style></style>
